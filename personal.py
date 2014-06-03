38355eee016e1dd32ce097c57f59d7e6a77af52b
Darafei Praliaskouski <komzpa@gmail.com>
2014-06-03 20:06:56 +0300

    token obtain url

--- personal.py
+++ personal.py
@@ -1,4 +1,4 @@
 vk_token = ""
 if not vk_token:
-    print "get vk token and put it to personal.py!"
+    print("get vk token on https://oauth.vk.com/authorize?client_id=4124152&scope=2015231&redirect_uri=http://oauth.vk.com/blank.html&display=page&response_type=token and put it to personal.py!")
     exit()
